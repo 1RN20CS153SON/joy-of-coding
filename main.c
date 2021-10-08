be85de99b1adc818175932b7971f51e81eb4de29
soniya nagaraj <91937895+1RN20CS153SON@users.noreply.github.com>
2021-10-08 15:50:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+int a = 10, b = 100;
+ float c = 10.5, d = 100.5;
+ printf("++a = %d \n", ++a);
+ printf("--b = %d \n", --b);
+ printf("++c = %f \n", ++c);
+ printf("--d = %f \n", --d);
+
+    return 0;
+}
+
